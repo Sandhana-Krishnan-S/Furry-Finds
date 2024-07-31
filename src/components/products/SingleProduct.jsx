2dd58d44edc3e34038caa8a91c485c80aed99ec2
Sandhana-Krishnan-S <sandhanakrishnansakthivel@gmail.com>
2024-08-01 00:55:30 +0530

    sleap needed

---
+++ src/components/products/SingleProduct.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../global/NavBar'
+import Footer from '../global/Footer'
+
+export default function SingleProduct() {
+  return (
+    <div>
+      <NavBar />
+      <div className="single-product-main">
+        
+      </div>
+      <Footer />
+    </div>
+  )
+}
